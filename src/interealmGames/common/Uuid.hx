babfc6a94615fb3fd4f8603f9a21c419b7c1e1f8
dmcblue <dmcblue@gmail.com>
2018-11-12 18:57:49 -0500

    Add uuid

---
+++ src/interealmGames/common/Uuid.hx
@@ -0,0 +1,16 @@
+package interealmGames.common;
+
+import com.hurlant.crypto.prng.Random;
+import com.hurlant.crypto.extra.UUID;
+
+/**
+ * Uiid 
+ */
+class Uuid 
+{
+	public static function v4():String {
+		var rand = new Random();
+		var uuid = UUID.generateRandom(rand);
+		return uuid.toString();
+	}
+}
